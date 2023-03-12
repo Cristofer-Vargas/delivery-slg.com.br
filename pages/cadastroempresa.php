975af9f6847dc06c2956c9d50ab658bd596bcbf7
Cristofer Vargas <vargascristofer97@gmail.com>
2023-03-12 19:28:10 -0300

    Menu media query

--- pages/cadastroempresa.php
+++ pages/cadastroempresa.php
@@ -6,7 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>cadastro de empresa</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="../style/all.css">
+  <link rel="stylesheet" href="../style/media-queries/all.css">
 
 </head>
 
@@ -16,12 +18,12 @@
   <main>
     <div class="max-width-page-limit">
       <section class="max-width-content-limit main-content">
-        asdasdsad
+        Conteúdo de Cadastro para empresa
       </section>
     </div>
   </main>
 
-  <?php include('../molde/footer.html');?>
+  <?php include('../molde/footer.html'); ?>
 
 </body>
 
