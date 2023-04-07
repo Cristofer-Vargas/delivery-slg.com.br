9ed2bc73c56b7eb1f68fe912f552ecb0533a6fdf
Cristofer Vargas <vargascristofer97@gmail.com>
2023-04-07 16:44:27 -0300

    Conexao com Banco de Dado. Arquivo Functions

---
+++ source/config/functions.php
@@ -0,0 +1,26 @@
+<?php
+// Funções de verificar sessão, conectar com banco de dados, menssagem de erro perssonalizado, etc
+
+function ConexaoBD()
+{
+  $HOST = 'localhost';
+  $USER = 'root';
+  $PASS = '';
+  $DBNAME = 'deliveryslg';
+
+  $dns = 'mysql:host=' . $HOST . ';dbname=' . $DBNAME;
+  $opcoes = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8',
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+  );
+
+  try {
+    $conexao = new PDO($dns, $USER, $PASS, $opcoes);
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conexao;
+
+  } catch (PDOException $ex) {
+    echo 'Erro ao conectar ao banco: ' . $ex->getMessage();
+  }
+}
