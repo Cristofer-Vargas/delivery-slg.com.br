15b659b62c717ab563ccb3c3127072a8bd7b0871
Cristofer Vargas <vargascristofer97@gmail.com>
2023-04-06 21:13:41 -0300

    produtos e tela login

--- pages/produtos.php
+++ pages/produtos.php
@@ -1,19 +1,24 @@
 <?php
-include_once('../includes/metas_gerais.php');
+
 ?>
 
+  <?php include_once('../includes/metas_gerais.php'); ?>
+
+  <link rel="stylesheet" href="../assets/style/produtos.css">
+
+  <link rel="stylesheet" href="../assets/style/media-queries/all.css">
 </head>
 
 <body>
   <?php include_once('../includes/header.php'); ?>
   <main>
 
-    <div class="max-width-page-limit">
-      <picture>
-        <source media="(max-width: 768px)" srcset="../assets/images/main/produtos/banner-delivery-pequeno-medio">
-        <img src="../assets/images/main/produtos/banner-delivery-medio-baixo.jpg" alt="Banner delivery slg A cozinha que você ama, entregue na sua casa.">
-      </picture>
-    </div>
+  <div class="max-width-page-limit">
+    <picture class="banner-delivery">
+      <source media="(max-width: 768px)" srcset="../assets/images/main/produtos/banner-delivery-pequeno-medio">
+      <img src="../assets/images/main/produtos/banner-delivery-medio-baixo.jpg" alt="Banner delivery slg A cozinha que você ama, entregue na sua casa.">
+    </picture>
+  </div>
 
   </main>
   <?php include_once('../includes/footer.php'); ?>
