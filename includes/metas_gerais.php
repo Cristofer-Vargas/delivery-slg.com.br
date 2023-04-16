fe7bacecba80eeaa3945a6e0b78b1c0250346f2e
Cristofer Vargas <vargascristofer97@gmail.com>
2023-04-16 13:31:48 -0300

    ajusantado metas gerias com bootstrap

--- includes/metas_gerais.php
+++ includes/metas_gerais.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -14,6 +10,9 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" 
   integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" 
   crossorigin="anonymous" referrerpolicy="no-referrer">
-  
-  <link rel="stylesheet" href="/delivery-slg.com.br/assets/style/all.css">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" 
+  rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
   <script src="/delivery-slg.com.br/assets/javascript/main.js" defer></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" defer integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
--- pages/cadastrousuario.php
+++ pages/cadastrousuario.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<?php
+    require_once('../includes/metas_gerais.php');
+?>
 
     <link rel="stylesheet" href="../assets/style/all.css">
     <link rel="stylesheet" href="../assets/style/login.css">
@@ -19,8 +14,8 @@
     <?php include_once('../includes/header.php'); ?>
 
     <?php
-    require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . "../source/classes/usuarios.class.php");
-    require_once(str_replace('\\', '/', dirname(__FILE__, 2)) . "../source/controller/cadusuario.controller.php");
+    require_once("../source/classes/usuarios.class.php");
+    require_once("../source/controller/cadusuario.controller.php");
 
     $usuarios = new Usuarios ();
 
@@ -97,9 +92,7 @@
     </main>
     <footer>
         <?php include_once('../includes/footer.php'); ?>
-
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
