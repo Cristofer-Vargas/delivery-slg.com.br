0b55c230ee92b84853354bb7482dfcee0e93e712
xnectunex <brunaborckdeoliveira@gmail.com>
2023-04-24 20:03:49 -0300

    formatacao

--- pages/alterasenhaempresa.php
+++ pages/alterasenhaempresa.php
@@ -1,91 +1,91 @@
 <form id="form_altera_senha" method="POST" class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-          <div class="modal-header">
-              <h5 class="modal-title" id="TituloEsqueciSenha">Esqueci minha senha</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body col-md-12 offset-md-12 flex column align-items-center">
-              <div class="form_align">
-                  <label for="E-mail">E-mail ou CPF:</label>
-                  <input type="text" class="cor input_altera_senha" id="email" name="email">
-              </div>
-              <div class="form_align">
-                  <label for="senha">Nova senha:</label>
-                  <input type="password" class="cor input_altera_senha" id="novasenha" name="senha">
-              </div>
-              <div class="form_align">
-                  <label for="senha">Confirme a nova senha:</label>
-                  <input type="password" class="cor input_altera_senha" id="confirmasenha" name="senha">
-              </div>
-              <?php
-                if (isset($_SESSION) && isset($_SESSION['mensagem'])) { ?>
-                  <div class="alert alert-danger" role="alert">
-                      <?= $_SESSION['mensagem']; ?>
-                  </div>
-              <?php unset($_SESSION['mensagem']);
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="TituloEsqueciSenha">Esqueci minha senha</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body col-md-12 offset-md-12 flex column align-items-center">
+            <div class="form_align">
+                <label for="E-mail">E-mail ou CPF:</label>
+                <input type="text" class="cor input_altera_senha" id="email" name="email">
+            </div>
+            <div class="form_align">
+                <label for="senha">Nova senha:</label>
+                <input type="password" class="cor input_altera_senha" id="novasenha" name="senha">
+            </div>
+            <div class="form_align">
+                <label for="senha">Confirme a nova senha:</label>
+                <input type="password" class="cor input_altera_senha" id="confirmasenha" name="senha">
+            </div>
+            <?php
+            if (isset($_SESSION) && isset($_SESSION['mensagem'])) { ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= $_SESSION['mensagem']; ?>
+                </div>
+            <?php unset($_SESSION['mensagem']);
+            }
+            ?>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-warning" onclick="save();">Salvar</button>
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal" aria-label="Close">Fechar</button>
+        </div>
+    </div>
+</form>
+
+
+<script>
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    function save() {
+        var inp = document.getElementsByClassName('input_altera_senha');
+        var form_Data = new FormData();
+        var novasenha = document.getElementById('novasenha');
+        var confirmasenha = document.getElementById('confirmasenha');
+
+        if (novasenha.value != confirmasenha.value) {
+            toastr.error('As senhas inseridas não são iguais.');
+            return;
+        }
+
+        for (let i = 0; i < inp.length; i++) {
+            form_Data.append(inp[i].name, inp[i].value);
+        }
+
+        fetch('../source/controller/alterarsenha.controller.php?tipo=empresa', {
+                method: 'post',
+                body: form_Data
+            }).then(res => res.json())
+            .then(res => {
+                console.log(res);
+                if (res.sucesso == true) {
+                    toastr.success(res.mensagem);
+                } else {
+                    toastr.error(res.mensagem);
                 }
-                ?>
-          </div>
-          <div class="modal-footer">
-              <button type="button" class="btn btn-warning" onclick="save();">Salvar</button>
-              <button type="button" class="btn btn-danger" data-bs-dismiss="modal" aria-label="Close">Fechar</button>
-          </div>
-      </div>
-  </form>
+            })
+            .catch(err => console.error('erro :: ', err))
+    }
 
 
-  <script>
-      toastr.options = {
-          "closeButton": false,
-          "debug": false,
-          "newestOnTop": false,
-          "progressBar": false,
-          "positionClass": "toast-bottom-right",
-          "preventDuplicates": false,
-          "onclick": null,
-          "showDuration": "300",
-          "hideDuration": "1000",
-          "timeOut": "5000",
-          "extendedTimeOut": "1000",
-          "showEasing": "swing",
-          "hideEasing": "linear",
-          "showMethod": "fadeIn",
-          "hideMethod": "fadeOut"
-      }
-
-      function save() {
-          var inp = document.getElementsByClassName('input_altera_senha');
-          var form_Data = new FormData();
-          var novasenha = document.getElementById('novasenha');
-          var confirmasenha = document.getElementById('confirmasenha');
-
-          if (novasenha.value != confirmasenha.value) {
-              toastr.error('As senhas inseridas não são iguais.');
-              return;
-          }
-
-          for (let i = 0; i < inp.length; i++) {
-              form_Data.append(inp[i].name, inp[i].value);
-          }
-
-          fetch('../source/controller/alterarsenha.controller.php?tipo=empresa', {
-                  method: 'post',
-                  body: form_Data
-              }).then(res => res.json())
-              .then(res => {
-                  console.log(res);
-                  if (res.sucesso == true) {
-                      toastr.success(res.mensagem);
-                  } else {
-                      toastr.error(res.mensagem);
-                  }
-              })
-              .catch(err => console.error('erro :: ', err))
-      }
-
-
-      document.getElementById('form_altera_senha')
-          .addEventListener('submit', ($event) => {
-              $event.preventDefault();
-          })
-  </script>
+    document.getElementById('form_altera_senha')
+        .addEventListener('submit', ($event) => {
+            $event.preventDefault();
+        })
+</script>
--- source/classes/restaurantes.class.php
+++ source/classes/restaurantes.class.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 
-class Restaurantes {
+class Restaurantes
+{
   private int $id;
   private string $nome;
   private string $rua;
@@ -18,132 +19,163 @@ class Restaurantes {
   private bool $ativo;
   private string $imagem;
 
-  public function getId(): int {
+  public function getId(): int
+  {
     return $this->id;
   }
 
-  public function setId(int $id) {
+  public function setId(int $id)
+  {
     $this->id = $id;
   }
-  
-  public function getNome(): string {
+
+  public function getNome(): string
+  {
     return $this->nome;
   }
 
-  public function setNome(string $nome) {
+  public function setNome(string $nome)
+  {
     $this->nome = $nome;
   }
-  
-  public function getRua(): string {
+
+  public function getRua(): string
+  {
     return $this->rua;
   }
 
-  public function setRua(string $rua) {
+  public function setRua(string $rua)
+  {
     $this->rua = $rua;
   }
-  
-  public function getNumero(): string {
+
+  public function getNumero(): string
+  {
     return $this->numero;
   }
 
-  public function setNumero(string $numero) {
+  public function setNumero(string $numero)
+  {
     $this->numero = $numero;
   }
-  
-  public function getComplemento(): string {
+
+  public function getComplemento(): string
+  {
     return $this->complemento;
   }
 
-  public function setComplemento(string $complemento) {
+  public function setComplemento(string $complemento)
+  {
     $this->complemento = $complemento;
   }
-  
-  public function getBairro(): string {
+
+  public function getBairro(): string
+  {
     return $this->bairro;
   }
 
-  public function setBairro(string $bairro) {
+  public function setBairro(string $bairro)
+  {
     $this->bairro = $bairro;
   }
-  
-  public function getEmail(): string {
+
+  public function getEmail(): string
+  {
     return $this->email;
   }
 
-  public function setEmail(string $email) {
+  public function setEmail(string $email)
+  {
     $this->email = $email;
   }
-  
-  public function getTelefone(): string {
+
+  public function getTelefone(): string
+  {
     return $this->telefone;
   }
 
-  public function setTelefone(string $telefone) {
+  public function setTelefone(string $telefone)
+  {
     $this->telefone = $telefone;
   }
-  
-  public function getCnpj(): string {
+
+  public function getCnpj(): string
+  {
     return $this->cnpj;
   }
 
-  public function setCnpj(string $cnpj) {
+  public function setCnpj(string $cnpj)
+  {
     $this->cnpj = $cnpj;
   }
-  
-  public function getHora_Funcionamento(): string {
+
+  public function getHora_Funcionamento(): string
+  {
     return $this->hora_Funcionamento;
   }
 
-  public function setHora_Funcionamento(string $hora_Funcionamento) {
+  public function setHora_Funcionamento(string $hora_Funcionamento)
+  {
     $this->hora_Funcionamento = $hora_Funcionamento;
   }
 
-  public function getFormas_Pagamento(): string {
+  public function getFormas_Pagamento(): string
+  {
     return $this->formas_Pagamento;
   }
 
-  public function setFormas_Pagamento(string $formas_Pagamento) {
+  public function setFormas_Pagamento(string $formas_Pagamento)
+  {
     $this->formas_Pagamento = $formas_Pagamento;
   }
 
-  public function getFormas_Entrega(): string {
+  public function getFormas_Entrega(): string
+  {
     return $this->formas_Entrega;
   }
 
-  public function setFormas_Entrega(string $formas_Entrega) {
+  public function setFormas_Entrega(string $formas_Entrega)
+  {
     $this->formas_Entrega = $formas_Entrega;
   }
-  
-  public function getChave_Pix(): string {
+
+  public function getChave_Pix(): string
+  {
     return $this->chave_Pix;
   }
 
-  public function setChave_Pix(string $chave_Pix) {
+  public function setChave_Pix(string $chave_Pix)
+  {
     $this->chave_Pix = $chave_Pix;
   }
-  
-  public function getSenha(): string {
+
+  public function getSenha(): string
+  {
     return $this->senha;
   }
 
-  public function setSenha(string $senha) {
+  public function setSenha(string $senha)
+  {
     $this->senha = $senha;
   }
-  
-  public function getAtivo(): bool {
+
+  public function getAtivo(): bool
+  {
     return $this->ativo;
   }
 
-  public function setAtivo(bool $ativo) {
+  public function setAtivo(bool $ativo)
+  {
     $this->ativo = $ativo;
   }
-  
-  public function getImagem(): string {
+
+  public function getImagem(): string
+  {
     return $this->imagem;
   }
 
-  public function setImagem(string $imagem) {
+  public function setImagem(string $imagem)
+  {
     $this->imagem = $imagem;
   }
-  
 }
